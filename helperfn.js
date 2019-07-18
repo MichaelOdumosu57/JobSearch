6e34e3a13aa00416c0ca623f67c83ae66bf2300f
Michael Odumosu <michaelodumosu57@gmail.com>
2019-07-17 20:21:48 -0400

    a link now

--- helperfn.js
+++ helperfn.js
@@ -1,18 +1,1 @@
-function toPhoneNumberString(   dev_obj   ){
-    var pNArray = dev_obj.string.split('')
-    var pNFinal = ''
-    pNArray.map((a)=>{
-        
-        
-        if(   a == parseInt(a)   ){
-            
-            
-            pNFinal += a
-            
-            
-        }
-        
-        
-    })
-    return pNFinal
-}
+./helperfn.js
